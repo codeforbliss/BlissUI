0fae3d6e6f994cdb416ca113a24affa354641dd7
QW66666 <wusteven0829@gmail.com>
2023-10-29 15:44:52 -0400

    added navigation bar and Posts Page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import QuoteContainer from './components/QuoteContainer';
+import {BrowserRouter as Router, Route, Link, Switch, Routes } from 'react-router-dom';
+import PostList from './components/PostList';
+import Layout from './components/Layout';
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
-      <header>
-        <title>Bliss</title>
-        <h1>Bliss</h1>
-        <div className="Quotes">
-        <QuoteContainer>
-        </QuoteContainer>
-        </div>
-      </header>
-    </div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route path= "postlist" element={<PostList />} />
+          <Route index element={<QuoteContainer />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+    
+    
   );
 }
 
