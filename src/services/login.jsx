9053dd75268abdd73b5ef7b9d1d1fe4de8cd2912
quntaoz <quntaozheng@gmail.com>
2024-06-22 23:30:18 -0400

    user reducer and quote reducer to save token

--- src/services/login.jsx
+++ src/services/login.jsx
@@ -2,20 +2,6 @@ import axios from 'axios'
 
 const baseUrl = "api/v1/auth/"
 
-let token = null
-
-const setToken = (newToken) => {
-  token = `Bearer ${newToken}`
-}
-
-// Axios request interceptor to add token to all requests
-axios.interceptors.request.use(config => {
-  config.headers.authorization = token
-  return config
-}, error => {
-  return Promise.reject(error)
-})
-
 const login = async (credential) => {
     const response = await axios.post(baseUrl + "authenticate", credential)
     return response.data
@@ -26,4 +12,9 @@ const signUp = async (credential) => {
   return response.data
 }
 
-export default {login, setToken, signUp}
+const checkToken = async (token) => {
+  const response = await axios.get(baseUrl + `expired?token=${token}`)
+  return response.data
+}
+
+export default {login, signUp, checkToken}
