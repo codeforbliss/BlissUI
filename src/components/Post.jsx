873e512d3df45d385d5ca74e2abc0635cae8fe23
QW66666 <wusteven0829@gmail.com>
2024-08-10 01:13:54 -0400

    added feature to display and make comments on posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { isValidUser } from '../reducer/userReducer';
 import postService from '../services/postService';
+import commentService from '../services/commentService'; // Service to fetch comments and replies
 import 'bootstrap/dist/css/bootstrap.css';
 import Card from 'react-bootstrap/Card';
 import Layout from './Navbar';
@@ -9,9 +10,11 @@ import '../assets/Post.css'; // Import the custom CSS file
 
 const Post = () => {
     const [posts, setPosts] = useState([]);
+    const [newComment, setNewComment] = useState({}); // Store new comments
+    const [newReply, setNewReply] = useState({}); // Store new replies
+    const [commentsMap, setCommentsMap] = useState({}); // Map of commentId to comments
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
-    const token = "Bearer " + useSelector((state) => state.user.token);
 
     useEffect(() => {
         dispatch(isValidUser());
@@ -21,19 +24,155 @@ const Post = () => {
         const fetchPosts = async () => {
             if (user.token) {
                 try {
-                    const postsData = await postService.getAllPosts(token);
-                    setPosts(postsData.data);
+                    const postsData = await postService.getAllPosts();
+                    const postsWithComments = await Promise.all(postsData.data.map(async (post) => {
+                        const comments = await commentService.getCommentsByIds(post.comments);
+                        return {
+                            ...post,
+                            comments: comments.data
+                        };
+                    }));
+                    setPosts(postsWithComments);
                 } catch (error) {
-                    console.error("Failed to fetch posts", error);
+                    console.error("Failed to fetch posts or comments", error);
                 }
             }
         };
         fetchPosts();
     }, [user.token]);
 
+
+    useEffect(() => {
+        const fetchCommentsRecursively = async (commentIds) => {
+            try {
+                const allComments = await commentService.getCommentsByIds(commentIds);
+                const comments = allComments.data;
+    
+                // Build a map of comments
+                const commentsMap = {};
+                for (const comment of comments) {
+                    commentsMap[comment.id] = comment;
+                    // Fetch replies if any
+                    if (comment.comments.length > 0) {
+                        const repliesMap = await fetchCommentsRecursively(comment.comments);
+                        Object.assign(commentsMap, repliesMap);
+                    }
+                }
+                return commentsMap;
+            } catch (error) {
+                console.error("Failed to fetch comments", error);
+                return {};
+            }
+        };
+    
+        const fetchAllComments = async () => {
+            const commentIds = posts.flatMap(post => post.comments.map(comment => comment.id)); 
+            if (commentIds.length > 0) {
+                const fetchedCommentsMap = await fetchCommentsRecursively(commentIds);
+                setCommentsMap(fetchedCommentsMap);
+            }
+        };
+        fetchAllComments();
+    }, [posts]);
+
+    const handleCommentChange = (postId, value) => {
+        setNewComment({
+            ...newComment,
+            [postId]: value
+        });
+    };
+
+    const handleAddComment = async (postId) => {
+        const commentText = newComment[postId];
+        if (!commentText) return;
+
+        try {
+            const commentData = {
+                text: commentText,
+                author: "anonymous person",
+                date: new Date(),
+                postId
+            };
+            const newComment = await postService.addCommentToPost(postId, commentData);
+            setNewComment({
+                ...newComment,
+                [postId]: ''
+            });
+        } catch (error) {
+            console.error("Failed to add comment", error);
+        }
+    };
+
+    const handleReplyChange = (commentId, value) => {
+        setNewReply({
+            ...newReply,
+            [commentId]: value
+        });
+    };
+
+    const handleAddReply = async (commentId) => {
+        const replyText = newReply[commentId];
+        if (!replyText) return;
+
+        try {
+            const replyData = {
+                text: replyText,
+                author: "anonymous person",
+                date: new Date(),
+                commentId
+            };
+            const newReply = await commentService.addReplyToComment(commentId, replyData);
+            setNewReply({
+                ...newReply,
+                [commentId]: ''
+            });
+        } catch (error) {
+            console.error("Failed to add reply", error);
+        }
+    };
+
+    const renderComments = (commentIds) => {
+        return (
+            <ul>
+                {commentIds.map((commentId) => {
+                    const comment = commentsMap[commentId];
+                    if (!comment) return null; // Skip if comment not found
+    
+                    return (
+                        <li key={comment.id}>
+                            <p>{comment.text}</p>
+                            <p className="blockquote-footer">
+                                {comment.author}
+                            </p>
+                            {/* Replies */}
+                            <div className="replies-section">
+                                {comment.comments.length > 0 ? (
+                                    renderComments(comment.comments)
+                                ) : (
+                                    <p>No replies yet</p>
+                                )}
+                                {/* Add Reply */}
+                                <div className="add-reply">
+                                    <input
+                                        type="text"
+                                        placeholder="Reply..."
+                                        value={newReply[comment.id] || ''}
+                                        onChange={(e) => handleReplyChange(comment.id, e.target.value)}
+                                    />
+                                    <button onClick={() => handleAddReply(comment.id)}>Reply</button>
+                                </div>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    };
+    
+
     return (
         <>
-            <Layout></Layout>
+            <Layout />
             <div className="post-container">
                 <h1 className="post-header">Posts</h1>
                 {Array.isArray(posts) && posts.map((post) => (
@@ -46,6 +185,24 @@ const Post = () => {
                                         {post.author}
                                     </footer>
                                 </blockquote>
+                                <div className="comments-section">
+                                    <h6>Comments:</h6>
+                                    {post.comments && post.comments.length > 0 ? (
+                                        renderComments(post.comments.map(comment => comment.id))
+                                    ) : (
+                                        <p>No comments yet</p>
+                                    )}
+                                    {/* Add Comment */}
+                                    <div className="add-comment">
+                                        <input
+                                            type="text"
+                                            placeholder="Add a comment..."
+                                            value={newComment[post.id] || ''}
+                                            onChange={(e) => handleCommentChange(post.id, e.target.value)}
+                                        />
+                                        <button onClick={() => handleAddComment(post.id)}>Post</button>
+                                    </div>
+                                </div>
                             </Card.Body>
                         </Card>
                     </div>
---
+++ src/services/commentService.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const baseUrl = 'api/comments';
+let token = null;
+
+const setToken = (newToken) => {
+    token = `Bearer ${newToken}`;
+}
+
+axios.interceptors.request.use(
+    (config) => {
+    config.headers.authorization = token;
+    return config;
+    },
+    (error) => {
+    return Promise.reject(error);
+    },
+);
+
+const addReplyToComment = async (id, comment) => {
+    try {
+        axios.post(baseUrl + "/comment/" + id, comment);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getCommentsByIds = async (ids) => {
+    try {
+        const response = await axios.post(`${baseUrl}/batch`, ids);
+        return response;
+    } catch (error) {
+        console.error("Failed to fetch comments", error);
+        throw error;
+    }
+};
+
+
+
+export default {addReplyToComment, getCommentsByIds, setToken}
